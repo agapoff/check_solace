49d6e101915b106f6c91da3223ce0e968a5a80ff
Vitaly Agapov <v.agapov@quotix.com>
2017-02-28 10:13:41 +0200

    Prepare Nagios-style plugin

--- check_solace.pl
+++ check_solace.pl
@@ -27,6 +27,7 @@ GetOptions(
     'critical=s',
     'version|V=s',
     'mode|m=s',
+	'name|n=s',
     'host|H=s',
     'port|p=s',
     'username|u=s',
@@ -112,6 +113,8 @@ elsif ($opt{mode} eq 'disk') {
 		my $perfdataOut = join ', ',@perfdata;
         print $ERROR{$exitStatus}.". ".$output." | ".$perfdataOut."\n";
 		exit $exitStatus;
+    } else {
+        fail($req->{error});
     }
 }
 elsif ($opt{mode} eq 'memory') {
@@ -126,128 +129,153 @@ elsif ($opt{mode} eq 'memory') {
         } elsif ( $physMemUsage >= $opt{warning} || $subscrMemUsage >= $opt{warning}) {
             $exitStatus = $CODE{WARNING};
         }
-        print $ERROR{$exitStatus}.". Physical mem usage $physMemUsage%. Subscriptions mem usage $subscrMemUsage% | 'physical-memory-usage'=$physMemUsage%;$opt{warning};$opt{critical}, 'subscription-memory-usage'=$subscrMemUsage%;$opt{warning};$opt{critical}";
+        print $ERROR{$exitStatus}.". Physical mem usage $physMemUsage%. Subscriptions mem usage $subscrMemUsage% | 'physical-memory-usage'=$physMemUsage%;$opt{warning};$opt{critical}, 'subscription-memory-usage'=$subscrMemUsage%;$opt{warning};$opt{critical}\n";
 		exit $exitStatus;
-	}
+	} else {
+        fail($req->{error});
+    }
 }
-
-print "\n Environment:\n";
-my $req = $semp->getEnvironment;
-if (! $req->{error} ) {
-    my $count = -1;
-    my $output = '';
-    foreach (@{$req->{result}->{status}}) {
-        $count++;
-        next if ($_ eq 'OK' || $_ eq '');
-        $output .= $req->{result}->{'type'}->[$count].' '.$req->{result}->{'name'}->[$count].' '.
+elsif ($opt{mode} eq 'environment') {
+    my $req = $semp->getEnvironment;
+    if (! $req->{error} ) {
+        my $count = -1;
+        my $output = '';
+        foreach (@{$req->{result}->{status}}) {
+            $count++;
+            next if ($_ eq 'OK' || $_ eq '');
+			$exitStatus = $CODE{CRITICAL};
+            $output .= $req->{result}->{'type'}->[$count].' '.$req->{result}->{'name'}->[$count].' '.
              $req->{result}->{'value'}->[$count].' '.$req->{result}->{'unit'}->[$count].' '.
              $req->{result}->{'status'}->[$count].'. ';
-    }
-    if ($output) {
-        print "CRITICAL. ".$output;
+        }
+		print "Environment ".$ERROR{$exitStatus}.' '.$output."\n";
+		exit $exitStatus;
     } else {
-        print "Environment OK";
+        fail($req->{error});
     }
-    print "\n";
 }
-
-print "\n Interface:\n";
-#my $interface = '1/6/lag1';
-my $interface = 'eth1';
-$req = $semp->getInterface(name => $interface);
-if (! $req->{error} ) {
-    my $output = '';
-    my $rxBytes = $req->{result}->{'rx-bytes'}->[0];
-    my $txBytes = $req->{result}->{'tx-bytes'}->[0];
-    my $rxPkts  = $req->{result}->{'rx-pkts'}->[0];
-    my $txPkts  = $req->{result}->{'tx-pkts'}->[0];
-    my $enabled = $req->{result}->{enabled}->[0];
-    my $mode    = $req->{result}->{mode}->[0];
-    my $link    = $req->{result}->{'link-detected'}->[0];
-    if ($rxBytes >= $opt{critical} || $txBytes >= $opt{critical}) {
-        $exitStatus = 2;
-    } elsif ( $rxBytes >= $opt{warning} || $txBytes >= $opt{warning}) {
-        $exitStatus = 1;
-    }
-    print "Exit status $exitStatus. Enabled: $enabled, ";
-    if ($mode) {
-        print "Mode: $mode, Operational Members: ".$req->{result}->{'operational-members'};
+elsif ($opt{mode} eq 'interface') {
+    if (! $opt{name} ) {
+	    print "Interface name not defined\n";
+		exit $CODE{CRITICAL};
+	}
+    my $req = $semp->getInterface(name => $opt{name});
+    if (! $req->{error} ) {
+        my $output = '';
+        my $rxBytes = $req->{result}->{'rx-bytes'}->[0];
+        my $txBytes = $req->{result}->{'tx-bytes'}->[0];
+        my $rxPkts  = $req->{result}->{'rx-pkts'}->[0];
+        my $txPkts  = $req->{result}->{'tx-pkts'}->[0];
+        my $enabled = $req->{result}->{enabled}->[0];
+        my $mode    = $req->{result}->{mode}->[0];
+        my $link    = $req->{result}->{'link-detected'}->[0];
+		if (! defined $enabled) {
+		    print "Interface $opt{name} not found\n";
+			exit $CODE{CRITICAL};
+		}
+        $output .= "Iface $opt{name}, Enabled: $enabled, ";
+        if ($mode) {
+		    my $members = $req->{result}->{'operational-members'};
+            $output .= "Mode: $mode, Operational Members: $members";
+			if (! $members > 0) { $exitStatus = $CODE{CRITICAL}; }
+        } else {
+            $output .= "Link: $link";
+            if ($link ne 'yes') { $exitStatus = $CODE{CRITICAL}; }
+        }
+        $output .= " | 'rx-bytes'=$rxBytes, 'tx-bytes'=$txBytes, 'rx-pkts'=$rxPkts, 'tx-pkts'=$txPkts";
+		print $ERROR{$exitStatus}.' '.$output."\n";
+		exit $exitStatus;
     } else {
-        print "Link: $link";
-        if ($link ne 'yes') { $exitStatus = 2; }
+        fail($req->{error});
     }
-    print " | 'rx-bytes'=$rxBytes, 'tx-bytes'=$txBytes, 'rx-pkts'=$rxPkts, 'tx-pkts'=$txPkts";
 }
+elsif ($opt{mode} eq 'clients') {
+    $opt{warning} ||= 1000;
+    $opt{critical} ||= 1000;
 
-print "\n Stats:\n";
-$req = $semp->getClientStats;
-if (! $req->{error} ) {
-    #print Dumper($req);
-    my $clients = $req->{result}->{'total-clients-connected'}->[0];
-    my $ingressRate = $req->{result}->{'average-ingress-rate-per-minute'}->[0];
-    my $egressRate = $req->{result}->{'average-egress-rate-per-minute'}->[0];
-    my $ingressByteRate = $req->{result}->{'average-ingress-byte-rate-per-minute'}->[0];
-    my $egressByteRate = $req->{result}->{'average-egress-byte-rate-per-minute'}->[0];
-    my $ingressDiscards = $req->{result}->{'total-ingress-discards'}->[0];
-    my $egressDiscards = $req->{result}->{'total-egress-discards'}->[0];
+    my $req = $semp->getClientStats;
+    if (! $req->{error} ) {
+        my $clients = $req->{result}->{'total-clients-connected'}->[0];
+        my $ingressRate = $req->{result}->{'average-ingress-rate-per-minute'}->[0];
+        my $egressRate = $req->{result}->{'average-egress-rate-per-minute'}->[0];
+        my $ingressByteRate = $req->{result}->{'average-ingress-byte-rate-per-minute'}->[0];
+        my $egressByteRate = $req->{result}->{'average-egress-byte-rate-per-minute'}->[0];
+        my $ingressDiscards = $req->{result}->{'total-ingress-discards'}->[0];
+        my $egressDiscards = $req->{result}->{'total-egress-discards'}->[0];
 
-    if ($clients >= $opt{critical}) {
-        $exitStatus = 2;
-    } elsif ( $clients >= $opt{warning}) {
-        $exitStatus = 1;
-    }
-    print "Exit status $exitStatus. $clients connected, Rate $ingressRate/$egressRate msg/sec, ".
-       "Discarded $ingressDiscards/$egressDiscards | 'connected'=$clients;$opt{warning};$opt{critical}, ".
-       "'ingress-rate'=$ingressRate, 'egress-rate'=$egressRate, 'ingress-byte-rate'=$ingressByteRate, ".
-       "'egress-byte-rate'=$egressByteRate, 'ingress-discards'=$ingressDiscards, 'egress-discards'=$egressDiscards";
-    
-}
-
-
-$req = $semp->getMessageVpnStats(name => "webtrader-ICONS");
-if (! $req->{error} ) {
-    if (! defined $req->{result}->{enabled}->[0]) {
-        print "UNKNOWN. Message VPN not known";
-        exit;
-    }
-    my $enabled = $req->{result}->{enabled}->[0];
-    my $operational = $req->{result}->{operational}->[0];
-    my $status = $req->{result}->{'local-status'}->[0];
-    my $uniqueSubscriptions = $req->{result}->{'unique-subscriptions'}->[0];
-    my $maxSubscriptions = $req->{result}->{'max-subscriptions'}->[0];
-    my $connections = $req->{result}->{'connections'}->[0];
-    my $maxConnections = $req->{result}->{'max-connections'}->[0];
-    my $connSMF = $req->{result}->{'connections-service-smf'}->[0];
-    my $connWEB = $req->{result}->{'connections-service-web'}->[0];
-    my $connMQTT = $req->{result}->{'connections-service-mqtt'}->[0];
-    my $ingressRate = $req->{result}->{'average-ingress-rate-per-minute'}->[0];
-    my $egressRate = $req->{result}->{'average-egress-rate-per-minute'}->[0];
-    my $ingressByteRate = $req->{result}->{'average-ingress-byte-rate-per-minute'}->[0];
-    my $egressByteRate = $req->{result}->{'average-egress-byte-rate-per-minute'}->[0];
-    my $ingressDiscards = $req->{result}->{'total-ingress-discards'}->[0];
-    my $egressDiscards = $req->{result}->{'total-egress-discards'}->[0];
-    if ($enabled eq 'true' && $operational eq 'true' && $status eq 'Up') {
-       my $connUsage = 0;
-       if ($maxConnections > 0) {
-          $connUsage = $connections / $maxConnections;
-       }
-       my $subscrUsage = 0;
-       if ($maxSubscriptions > 0) {
-          $subscrUsage = $uniqueSubscriptions / $maxSubscriptions;
-       }
-       if ($connUsage >= $opt{critical} || $subscrUsage >= $opt{critical}) {
-          $exitStatus = 2;
-       } elsif ($connUsage >= $opt{warning} || $subscrUsage >= $opt{warning}) {
-          $exitStatus = 1;
-       }
-       print "Exit status $exitStatus. Subscriptions $uniqueSubscriptions/$maxSubscriptions, ".
-          "Connections $connections/$maxConnections | 'unique-subscriptions'=$uniqueSubscriptions, ".
-          "'subscriptions-usage'=$subscrUsage%, 'connections'=$connections, 'conn-usage'=$connUsage, ".
-          "'conn-smf'=$connSMF, 'conn-web'=$connWEB, 'conn-mqtt'=$connMQTT, ". 
+        if ($clients >= $opt{critical}) {
+            $exitStatus = $CODE{CRITICAL};
+        } elsif ( $clients >= $opt{warning}) {
+            $exitStatus = $CODE{WARNING};
+        }
+        print $ERROR{$exitStatus}.". $clients connected, Rate $ingressRate/$egressRate msg/sec, ".
+          "Discarded $ingressDiscards/$egressDiscards | 'connected'=$clients;$opt{warning};$opt{critical}, ".
           "'ingress-rate'=$ingressRate, 'egress-rate'=$egressRate, 'ingress-byte-rate'=$ingressByteRate, ".
-          "'egress-byte-rate'=$egressByteRate, 'ingress-discards'=$ingressDiscards, 'egress-discards'=$egressDiscards";
+          "'egress-byte-rate'=$egressByteRate, 'ingress-discards'=$ingressDiscards, 'egress-discards'=$egressDiscards\n";
+		exit $exitStatus;
     } else {
-       print "CRITICAL. Enabled: $enabled, Operational: $operational, Status: $status";
+        fail($req->{error});
+    }
+}
+elsif ($opt{mode} eq 'vpn') {
+    $opt{warning} ||= 50;
+    $opt{critical} ||= 95;
+
+    if (! $opt{name} ) {
+        print "Message-vpn name not defined\n";
+        exit $CODE{CRITICAL};
+    }
+
+    my $req = $semp->getMessageVpnStats(name => $opt{name});
+    if (! $req->{error} ) {
+        my $enabled = $req->{result}->{enabled}->[0];
+        my $operational = $req->{result}->{operational}->[0];
+        my $status = $req->{result}->{'local-status'}->[0];
+        my $uniqueSubscriptions = $req->{result}->{'unique-subscriptions'}->[0];
+        my $maxSubscriptions = $req->{result}->{'max-subscriptions'}->[0];
+        my $connections = $req->{result}->{'connections'}->[0];
+        my $maxConnections = $req->{result}->{'max-connections'}->[0];
+        my $connSMF = $req->{result}->{'connections-service-smf'}->[0];
+        my $connWEB = $req->{result}->{'connections-service-web'}->[0];
+        my $connMQTT = $req->{result}->{'connections-service-mqtt'}->[0];
+        my $ingressRate = $req->{result}->{'average-ingress-rate-per-minute'}->[0];
+        my $egressRate = $req->{result}->{'average-egress-rate-per-minute'}->[0];
+        my $ingressByteRate = $req->{result}->{'average-ingress-byte-rate-per-minute'}->[0];
+        my $egressByteRate = $req->{result}->{'average-egress-byte-rate-per-minute'}->[0];
+        my $ingressDiscards = $req->{result}->{'total-ingress-discards'}->[0];
+        my $egressDiscards = $req->{result}->{'total-egress-discards'}->[0];
+
+        if (! defined $enabled) {
+	        print "Message VPN $opt{name} not known\n";
+	        exit $CODE{CRITICAL};
+        }
+
+        if ($enabled eq 'true' && $operational eq 'true' && $status eq 'Up') {
+            my $connUsage = 0;
+            if ($maxConnections > 0) {
+                $connUsage = sprintf("%.2f",$connections / $maxConnections);
+            }
+            my $subscrUsage = 0;
+            if ($maxSubscriptions > 0) {
+                $subscrUsage = sprintf("%.2f",$uniqueSubscriptions / $maxSubscriptions);
+            }
+            if ($connUsage >= $opt{critical} || $subscrUsage >= $opt{critical}) {
+                $exitStatus = $CODE{CRITICAL};
+            } elsif ($connUsage >= $opt{warning} || $subscrUsage >= $opt{warning}) {
+                $exitStatus = $CODE{WARNING};
+            }
+            print $ERROR{$exitStatus}.". Subscriptions $uniqueSubscriptions/$maxSubscriptions, ".
+             "Connections $connections/$maxConnections | 'unique-subscriptions'=$uniqueSubscriptions, ".
+             "'subscriptions-usage'=$subscrUsage%, 'connections'=$connections, 'conn-usage'=$connUsage, ".
+             "'conn-smf'=$connSMF, 'conn-web'=$connWEB, 'conn-mqtt'=$connMQTT, ". 
+             "'ingress-rate'=$ingressRate, 'egress-rate'=$egressRate, 'ingress-byte-rate'=$ingressByteRate, ".
+             "'egress-byte-rate'=$egressByteRate, 'ingress-discards'=$ingressDiscards, 'egress-discards'=$egressDiscards\n";
+        } else {
+            print "CRITICAL. Enabled: $enabled, Operational: $operational, Status: $status\n";
+			exit $CODE{CRITICAL};
+        }
+	} else {
+        fail($req->{error});
     }
 }
 
@@ -271,6 +299,7 @@ Common connection options:
  -P,  --password=PASS   management user password; defaults to 'admin'
  -V,  --version=NUM     Solace version (i.e. 8.0)
  -m,  --mode=STRING     test to perform
+ -n,  --name=STRING     name of the interface or message-vpn to test (needed when the corresponding mode is selected)
  -D,  --debug           debug mode
 
 Limit options:
@@ -282,6 +311,10 @@ Modes:
   alarm
   raid
   disk
+  memory
+  interface
+  clients
+  vpn
 };
    exit 0;
 }
